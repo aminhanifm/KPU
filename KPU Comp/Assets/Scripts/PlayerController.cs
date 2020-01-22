659df2ff2c908dcac6e731895a1604ed5942d57b
Amin Hanif Mahmud <aminhanif24@gmail.com>
2020-01-22 21:49:05 +0700

    Added .vs & asset folder to exception & added player animations

---
+++ KPU Comp/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Animator playeranimator;
+    private Rigidbody2D playerrb;
+
+    public float moveSpeed = 2f;
+
+    Vector2 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playeranimator = gameObject.GetComponent<Animator>();
+        playerrb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //testing only
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        playeranimator.SetFloat("Horizontal", movement.x);
+        playeranimator.SetFloat("Vertical", movement.y);
+        playeranimator.SetFloat("Speed", movement.sqrMagnitude);
+
+    }
+
+    public void FixedUpdate()
+    {
+        playerrb.MovePosition(playerrb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
